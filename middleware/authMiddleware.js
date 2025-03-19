3d8fad56c889c3f074dac25afd2260d2be1fb474
praveensivarj <praveensivarj3@gmail.com>
2025-03-19 17:55:17 +0530

    changes happen

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,11 +2,13 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization");
-  if (!token) return res.status(401).json({
-    success: false,
-    code: 401,
-    message: "Access Denied"
-  });
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      code: 401,
+      message: "Access Denied"
+    });
+  }
   try {
     const verified = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
     req.student = verified;
