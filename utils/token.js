a95ff7ab912db709c28e6003c2952f6266ba5db4
praveensivarj <praveensivarj3@gmail.com>
2025-03-28 16:43:55 +0530

    deleted utils folder

--- utils/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-module.exports = (user) => {
-  return jwt.sign(
-    { email: user.email, name: user.name },
-    process.env.JWT_SECRET,
-    { expiresIn: "1h" }
-  );
-};
